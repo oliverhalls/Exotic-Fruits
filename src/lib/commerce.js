83eb826771f0e722222ff7c9f5d9b413dc999f07
oliverhalls <oliver.15@hotmail.com>
2021-04-07 18:08:29 -0500

    Primer deploy

---
+++ src/lib/commerce.js
@@ -0,0 +1,6 @@
+import Commerce from '@chec/commerce.js';
+
+const COMMERCE_JS_PUBLIC_KEY='pk_test_25069ccf4ae78a64b1811327c0cc8b3a75f9fe3dc41d7'
+
+export const commerce = new Commerce(COMMERCE_JS_PUBLIC_KEY, true);
+
